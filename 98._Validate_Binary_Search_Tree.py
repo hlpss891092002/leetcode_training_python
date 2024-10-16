c8fa6b4261e3ff13feb8d802c6d6b8c5eb056d09
hlpss891092002 <hlpss891092002@gmail.com>
2024-10-16 22:01:38 +0800

    tree problem 98 & 101

--- 98._Validate_Binary_Search_Tree.py
+++ 98._Validate_Binary_Search_Tree.py
@@ -5,10 +5,14 @@ class TreeNode:
         self.right = right
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        return self._is_validBST(root, -2147483648, 2147483648)
-    def _is_validBST(self, root: TreeNode, min, max):
+        return self.validate_node(root, float("-inf"), float("-inf")) 
+        # float("inf") positive infinite  float("-inf") negative infinite 
+    def validate_node(self, root: TreeNode, min, max):
         if not root:
             return True
+        # beyond leaf node 
         if root.val <= min or root.val >= max:
             return False
-        return self._is_validBST(root.left, min, root.val) and self._is_validBST(root.right, root.val,max)
+        # out of compare range
+        return self.validate_node(root.left, min, root.val) and self.validate_node(root.right, root.val,max)
+        #  Setting node.val as max of left subtree  and min of right subtree, then traverse subtrees ; 
